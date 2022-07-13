61b989a8e48202c4c6888341d58a5e6c39785873
Vu Van Tai <vutai13196@gmail.com>
2022-07-13 21:11:07 +0700

    print list array

--- src/App.js
+++ src/App.js
@@ -7,9 +7,23 @@ import { useState } from 'react';
 function App() {
   let [name, setName] = useState('TaiVu')
   const [adress, setAdress] = useState('')
+  const [todos, setTodos] = useState([
+    { id: 'todo1', title: 'Taivu' },
+    { id: 'todo2', title: 'BabyShark' },
+    { id: 'todo3', title: 'Potter' },
+  ])
 
-  const handleOnclickButton = (event) => {
-    setName(adress)
+  const handleOnclickButton = () => {
+    if (!adress) {
+      alert('invalid new todo');
+      return;
+    }
+    let newTodo = {
+      id: Math.floor(Math.random() * 10001),
+      title: adress
+    }
+    setTodos([...todos, newTodo])
+    setAdress("")
   }
 
   const handleOnchangeInput = (event) => {
@@ -21,6 +35,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with babyShark - {name}</h1>
+        <div className='list-todo-container' >
+          {
+            todos.map((item) => {
+              return (
+                <li className='child' key={item.id}>{item.title}</li>
+              )
+            })
+          }
+        </div>
+
         <input type="text" value={adress} onChange={(event) => handleOnchangeInput(event)} />
         <button type="button" onClick={(event) => handleOnclickButton(event)}>click me</button>
       </header>
