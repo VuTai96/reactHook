d5f88a5a68660f6c6efc542aff446c3a13d6c829
Vu Van Tai <vutai13196@gmail.com>
2022-07-15 21:08:16 +0700

    different redirect with useHistory

--- src/views/BlogDetail.js
+++ src/views/BlogDetail.js
@@ -1,9 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useParams, useHistory, Link } from "react-router-dom";
 
 const BlogDetail = () => {
     let { id } = useParams()
+    let history = useHistory()
+    const handleOnclickButton = () => {
+        console.log(history);
+        history.push('/blog')
+    }
     return (
-        <h1>view detail {id}</h1>
+        <>
+            <h1>view detail {id}</h1>
+            <div><button onClick={handleOnclickButton}> Back</button> </div>
+            {/* <div><button> <Link to='/blog'>Back</Link></button> </div> */}
+        </>
     )
 }
 export default BlogDetail;
